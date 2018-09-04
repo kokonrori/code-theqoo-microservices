09c3a2c61e094e9a4ea13e9d154d0697f9afc536
jihun.choi <kokonrori@gmail.com>
2018-09-04 21:34:34 +0900

    Add postgres setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,7 @@
 
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -12,6 +13,24 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+# routes
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
