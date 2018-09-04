cae05413bce07156920b12054b902384f760ce64
jihun.choi <kokonrori@gmail.com>
2018-09-04 15:16:23 +0900

    Getting started

---
+++ services/users/project/__init__.py
@@ -0,0 +1,18 @@
+# services/users/project/__init__.py
+
+
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
