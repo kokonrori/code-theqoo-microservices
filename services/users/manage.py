a9485bd99b3c2583b01b9446b857c9ec61a33a16
jihun.choi <kokonrori@gmail.com>
2018-09-05 08:26:09 +0900

    Add users test setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,6 +1,8 @@
 # services/users/manage.py
 
 
+import unittest
+
 from flask.cli import FlaskGroup
 
 from project import app, db
@@ -14,6 +16,16 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """ Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
