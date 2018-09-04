5bddb05e229f910330ea6ccd13bf5387b86719c0
jihun.choi <kokonrori@gmail.com>
2018-09-04 16:54:02 +0900

    Add development docker config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 # services/users/project/__init__.py
 
 
+import os
 from flask import Flask, jsonify
 
 
@@ -8,7 +9,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
